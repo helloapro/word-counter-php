dd217cccf168bef9534522b7ab0574f8062b54de
Mark Lawson <mlawson3691@gmail.com>
2016-09-16 09:09:54 -0700

    Write test for second spec - match two letters

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -5,16 +5,24 @@
     {
         function test_countRepeats_return()
         {
-            // Arrange
             $test_RepeatCounter = new RepeatCounter;
             $input_text = 'a';
             $input_word = 'b';
 
-            // Act
             $result = $test_RepeatCounter->countRepeats($input_text, $input_word);
 
-            // Assert
             $this->assertEquals(0, $result);
+        }
+
+        function test_countRepeats_match()
+        {
+            $test_RepeatCounter = new RepeatCounter;
+            $input_text = 'a';
+            $input_word = 'a';
+
+            $result = $test_RepeatCounter->countRepeats($input_text, $input_word);
+
+            $this->assertEquals(1, $result);
         }
     }
 ?>
