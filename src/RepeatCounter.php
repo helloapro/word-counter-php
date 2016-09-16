34d9cf9d58eaa4457c254a7f2240beada1ca08b6
Mark Lawson <mlawson3691@gmail.com>
2016-09-16 09:24:45 -0700

    Make both inputs lowercase prior to searching for matches to pass fifth test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -4,7 +4,8 @@
         function countRepeats($input_text, $input_word)
         {
             $wordCount = 0;
-            $input_array = explode(" ", $input_text);
+            $input_word = strtolower($input_word);
+            $input_array = explode(" ", strtolower($input_text));
 
             foreach ($input_array as $word) {
                 if ($word == $input_word) {
