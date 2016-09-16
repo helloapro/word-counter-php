1e966c4a83964b6312d1a3b5b0acc094ef7de9c7
Mark Lawson <mlawson3691@gmail.com>
2016-09-16 09:20:27 -0700

    Pass the fourth test by incrementing the wordCount variable every time a match is found

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,17 +3,16 @@
     {
         function countRepeats($input_text, $input_word)
         {
-            $output = 0;
-
+            $wordCount = 0;
             $input_array = explode(" ", $input_text);
 
             foreach ($input_array as $word) {
                 if ($word == $input_word) {
-                    $output = 1;
+                    $wordCount += 1;
                 }
             }
 
-            return $output;
+            return $wordCount;
         }
     }
 ?>
