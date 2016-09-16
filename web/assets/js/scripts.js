72b11d73b6202645cba509feb427d6dca9da7956
Mark Lawson <mlawson3691@gmail.com>
2016-09-16 12:07:53 -0700

    Add a loop to remove punctuation before comparing to the search term

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,9 +1,19 @@
 $(function() {
     var wordArray = $('#text').text().split(' ');
     var searchTerm = $('#word').text();
+    var commonPunctuation = ['.', ',', '?', '!', ';', ':', '-', '/', '"', "'"];
+    var compareWord;
 
     $.each(wordArray, function(i, word) {
-        if (word.toLowerCase() == searchTerm.toLowerCase()) {
+        compareWord = word;
+        $.each(commonPunctuation, function(i, mark) {
+            if (word.indexOf(mark) >= 0) {
+                var str1 = word.substring(0, word.indexOf(mark));
+                var str2 = word.substring(word.indexOf(mark)+1, word.length);
+                compareWord = str1 + str2;
+            }
+        });
+        if (compareWord.toLowerCase() == searchTerm.toLowerCase()) {
             wordArray[i] = '<strong>' + word + '</strong>';
         }
     });
