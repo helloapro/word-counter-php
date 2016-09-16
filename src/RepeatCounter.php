12abbc560564c98964ee76b60d246241e57dd446
Mark Lawson <mlawson3691@gmail.com>
2016-09-16 09:07:06 -0700

    Create RepeatCounter class with countRepeats method that passes first test

---
+++ src/RepeatCounter.php
@@ -0,0 +1,11 @@
+<?php
+    class RepeatCounter
+    {
+        function countRepeats($input_text, $input_word)
+        {
+            $output = 0;
+            
+            return $output;
+        }
+    }
+?>
